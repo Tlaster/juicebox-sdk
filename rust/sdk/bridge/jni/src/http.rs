e9e6f34c533d37c931b26c64a4a45ca214a51a8e
Tlaster <tlaster@outlook.com>
2026-06-15 11:49:06 +0900

    Handle JVM JNI HTTP bridge exceptions

--- rust/sdk/bridge/jni/src/http.rs
+++ rust/sdk/bridge/jni/src/http.rs
@@ -56,71 +56,139 @@ impl HttpClient {
 impl sdk::http::Client for HttpClient {
     async fn send(&self, request: sdk::http::Request) -> Option<sdk::http::Response> {
         let (tx, rx) = channel();
+        let mut id = [0u8; 16];
+        OsRng.fill_bytes(&mut id);
 
         {
-            let mut env = self.jvm.attach_current_thread().unwrap();
+            let mut env = match self.jvm.attach_current_thread() {
+                Ok(env) => env,
+                Err(_) => return None,
+            };
 
-            let java_request_class = env.find_class(JUICEBOX_JNI_HTTP_REQUEST_TYPE).unwrap();
-            let java_request = env
-                .new_object(java_request_class, jni_signature!(() => JNI_VOID_TYPE), &[])
-                .unwrap();
+            let java_request_class = match env.find_class(JUICEBOX_JNI_HTTP_REQUEST_TYPE) {
+                Ok(value) => value,
+                Err(_) => {
+                    clear_pending_exception(&mut env);
+                    return None;
+                }
+            };
+            let java_request = match env.new_object(
+                java_request_class,
+                jni_signature!(() => JNI_VOID_TYPE),
+                &[],
+            ) {
+                Ok(value) => value,
+                Err(_) => {
+                    clear_pending_exception(&mut env);
+                    return None;
+                }
+            };
 
-            let mut id = [0u8; 16];
-            OsRng.fill_bytes(&mut id);
+            if set_byte_array(&mut env, &java_request, "id", &id).is_err() {
+                clear_pending_exception(&mut env);
+                return None;
+            }
+
+            if set_string(&mut env, &java_request, "method", request.method.as_str()).is_err() {
+                clear_pending_exception(&mut env);
+                return None;
+            }
+
+            if set_string(&mut env, &java_request, "url", request.url.as_str()).is_err() {
+                clear_pending_exception(&mut env);
+                return None;
+            }
+
+            if let Some(body) = request.body {
+                if set_byte_array(&mut env, &java_request, "body", &body).is_err() {
+                    clear_pending_exception(&mut env);
+                    return None;
+                }
+            }
+
+            let mut headers_array: Option<JObjectArray> = None;
+
+            if !request.headers.is_empty() {
+                let java_header_class = match env.find_class(JUICEBOX_JNI_HTTP_HEADER_TYPE) {
+                    Ok(value) => value,
+                    Err(_) => {
+                        clear_pending_exception(&mut env);
+                        return None;
+                    }
+                };
+
+                for (index, (name, value)) in request.headers.iter().enumerate() {
+                    let java_header = match env.new_object(
+                        &java_header_class,
+                        jni_signature!(() => JNI_VOID_TYPE),
+                        &[],
+                    ) {
+                        Ok(value) => value,
+                        Err(_) => {
+                            clear_pending_exception(&mut env);
+                            return None;
+                        }
+                    };
+
+                    if set_string(&mut env, &java_header, "name", name).is_err() {
+                        clear_pending_exception(&mut env);
+                        return None;
+                    }
+
+                    if set_string(&mut env, &java_header, "value", value).is_err() {
+                        clear_pending_exception(&mut env);
+                        return None;
+                    }
+
+                    match &headers_array {
+                        Some(array) => {
+                            if env
+                                .set_object_array_element(
+                                    array,
+                                    index.try_into().ok()?,
+                                    java_header,
+                                )
+                                .is_err()
+                            {
+                                clear_pending_exception(&mut env);
+                                return None;
+                            }
+                        }
+                        None => {
+                            headers_array = match env.new_object_array(
+                                request.headers.len().try_into().ok()?,
+                                JUICEBOX_JNI_HTTP_HEADER_TYPE,
+                                java_header,
+                            ) {
+                                Ok(value) => Some(value),
+                                Err(_) => {
+                                    clear_pending_exception(&mut env);
+                                    return None;
+                                }
+                            };
+                        }
+                    };
+                }
+            }
+
+            if let Some(array) = headers_array {
+                if env
+                    .set_field(
+                        &java_request,
+                        "headers",
+                        jni_array!(jni_object!(JUICEBOX_JNI_HTTP_HEADER_TYPE)),
+                        JValue::Object(&array),
+                    )
+                    .is_err()
+                {
+                    clear_pending_exception(&mut env);
+                    return None;
+                }
+            }
 
             {
                 let mut request_map = self.request_map.lock().unwrap();
                 request_map.insert(id, tx);
-            }
-
-            set_byte_array(&mut env, &java_request, "id", &id);
-
-            set_string(&mut env, &java_request, "method", request.method.as_str());
-
-            set_string(&mut env, &java_request, "url", request.url.as_str());
-
-            if let Some(body) = request.body {
-                set_byte_array(&mut env, &java_request, "body", &body);
-            }
-
-            let java_header_class = env.find_class(JUICEBOX_JNI_HTTP_HEADER_TYPE).unwrap();
-
-            let mut headers_array: Option<JObjectArray> = None;
-
-            for (index, (name, value)) in request.headers.iter().enumerate() {
-                let java_header = env
-                    .new_object(&java_header_class, jni_signature!(() => JNI_VOID_TYPE), &[])
-                    .unwrap();
-
-                set_string(&mut env, &java_header, "name", name);
-                set_string(&mut env, &java_header, "value", value);
-
-                match &headers_array {
-                    Some(array) => {
-                        env.set_object_array_element(array, index.try_into().unwrap(), java_header)
-                            .unwrap();
-                    }
-                    None => {
-                        headers_array = Some(
-                            env.new_object_array(
-                                request.headers.len().try_into().unwrap(),
-                                JUICEBOX_JNI_HTTP_HEADER_TYPE,
-                                java_header,
-                            )
-                            .unwrap(),
-                        );
-                    }
-                };
-            }
-
-            if let Some(array) = headers_array {
-                env.set_field(
-                    &java_request,
-                    "headers",
-                    jni_array!(jni_object!(JUICEBOX_JNI_HTTP_HEADER_TYPE)),
-                    JValue::Object(&array),
-                )
-                .unwrap();
             }
 
             let send_result = env.call_method(
@@ -134,6 +202,7 @@ impl sdk::http::Client for HttpClient {
             );
 
             if send_result.is_err() {
+                clear_pending_exception(&mut env);
                 self.remove(&id);
                 return None;
             }
@@ -143,24 +212,40 @@ impl sdk::http::Client for HttpClient {
     }
 }
 
-fn set_string(env: &mut JNIEnv, obj: &JObject, name: &str, string: &str) {
-    let java_string = env.new_string(string).unwrap();
+fn set_string(
+    env: &mut JNIEnv,
+    obj: &JObject,
+    name: &str,
+    string: &str,
+) -> jni::errors::Result<()> {
+    let java_string = env.new_string(string)?;
     env.set_field(
         obj,
         name,
         jni_object!(JNI_STRING_TYPE),
         JValue::Object(&java_string),
-    )
-    .unwrap();
+    )?;
+    Ok(())
 }
 
-fn set_byte_array(env: &mut JNIEnv, obj: &JObject, name: &str, array: &[u8]) {
-    let java_array = env.byte_array_from_slice(array).unwrap();
+fn set_byte_array(
+    env: &mut JNIEnv,
+    obj: &JObject,
+    name: &str,
+    array: &[u8],
+) -> jni::errors::Result<()> {
+    let java_array = env.byte_array_from_slice(array)?;
     env.set_field(
         obj,
         name,
         jni_array!(JNI_BYTE_TYPE),
         JValue::Object(&java_array),
-    )
-    .unwrap();
+    )?;
+    Ok(())
+}
+
+fn clear_pending_exception(env: &mut JNIEnv) {
+    if env.exception_check().unwrap_or(false) {
+        let _ = env.exception_clear();
+    }
 }
